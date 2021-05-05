7cee209b7146ff5138577d4ef7380bca140811ab
coramirezva <camilorava@gmail.com>
2021-05-04 19:05:50 -0500

    Se crea Proyecto Cliente en Spring Boot WebService Soap

---
+++ src/main/java/com/dataaccess/webservicesserver/NumberToDollars.java
@@ -0,0 +1,72 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.05.04 a las 06:58:45 PM COT 
+//
+
+
+package com.dataaccess.webservicesserver;
+
+import java.math.BigDecimal;
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="dNum" type="{http://www.w3.org/2001/XMLSchema}decimal"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "dNum"
+})
+@XmlRootElement(name = "NumberToDollars")
+public class NumberToDollars {
+
+    @XmlElement(required = true)
+    protected BigDecimal dNum;
+
+    /**
+     * Obtiene el valor de la propiedad dNum.
+     * 
+     * @return
+     *     possible object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public BigDecimal getDNum() {
+        return dNum;
+    }
+
+    /**
+     * Define el valor de la propiedad dNum.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link BigDecimal }
+     *     
+     */
+    public void setDNum(BigDecimal value) {
+        this.dNum = value;
+    }
+
+}
---
+++ src/main/java/com/dataaccess/webservicesserver/NumberToDollarsResponse.java
@@ -0,0 +1,71 @@
+//
+// Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.8-b130911.1802 
+// Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
+// Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
+// Generado el: 2021.05.04 a las 06:58:45 PM COT 
+//
+
+
+package com.dataaccess.webservicesserver;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
+import javax.xml.bind.annotation.XmlType;
+
+
+/**
+ * <p>Clase Java para anonymous complex type.
+ * 
+ * <p>El siguiente fragmento de esquema especifica el contenido que se espera que haya en esta clase.
+ * 
+ * <pre>
+ * &lt;complexType>
+ *   &lt;complexContent>
+ *     &lt;restriction base="{http://www.w3.org/2001/XMLSchema}anyType">
+ *       &lt;sequence>
+ *         &lt;element name="NumberToDollarsResult" type="{http://www.w3.org/2001/XMLSchema}string"/>
+ *       &lt;/sequence>
+ *     &lt;/restriction>
+ *   &lt;/complexContent>
+ * &lt;/complexType>
+ * </pre>
+ * 
+ * 
+ */
+@XmlAccessorType(XmlAccessType.FIELD)
+@XmlType(name = "", propOrder = {
+    "numberToDollarsResult"
+})
+@XmlRootElement(name = "NumberToDollarsResponse")
+public class NumberToDollarsResponse {
+
+    @XmlElement(name = "NumberToDollarsResult", required = true)
+    protected String numberToDollarsResult;
+
+    /**
+     * Obtiene el valor de la propiedad numberToDollarsResult.
+     * 
+     * @return
+     *     possible object is
+     *     {@link String }
+     *     
+     */
+    public String getNumberToDollarsResult() {
+        return numberToDollarsResult;
+    }
+
+    /**
+     * Define el valor de la propiedad numberToDollarsResult.
+     * 
+     * @param value
+     *     allowed object is
+     *     {@link String }
+     *     
+     */
+    public void setNumberToDollarsResult(String value) {
+        this.numberToDollarsResult = value;
+    }
+
+}
